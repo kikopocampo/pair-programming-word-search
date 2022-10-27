faca170de9916d110b1d535f813b4cd86a9a532f
Francisco <kikopocampo@gmail.com>
2022-10-26 19:38:32 -0600

    debugged function

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,69 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it ("Should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ASYH')
+
+    assert.isTrue(result);
+  })
+  it ("Should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'DLEF')
+
+    assert.isTrue(result);
+  })
+
+  it ("Should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'WMCN')
+
+    assert.isTrue(result);
+  })
+
+  it ("Should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'QTCF')
+
+    assert.isTrue(result);
+  })
+
+  
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,67 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
+  
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) return true;
+      if (l.split("").reverse().join("").includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+    const verticalJoin = changeVertial(letters).map((ls) => ls.join(""));
+  
+    for (let l of verticalJoin) {
+      if (l.includes(word)) return true;
+      if (l.split("").reverse().join("").includes(word)) return true;
+    }
+  
+    const diagonalJoin = changeDiagonal(letters).map((ls) => ls.join(""));
+  
+    for (let l of diagonalJoin) {
+      if (l.includes(word)) return true;
+      if (l.split("").reverse().join("").includes(word)) return true;
+    }
+  
+    return false;
+  };
+  
+  //const change vertical
+  const changeVertial = (arr) => {
+    let result = [];
+    for (let i = 0; i < arr[0].length; i++) {
+      let col = [];
+      for (let j = 0; j < arr.length; j++) {
+        col.push(arr[j][i]);
+      }
+      result.push(col);
+    }
+    return result;
+  };
+  
+  const changeDiagonal = (arr) => {
+    let result = [];
+    
+    for (let k = 0; k <= arr.length - 1; k++) {
+      const arr1 = [];
+      let i = k;
+      let j = 0;
+      while (i >= 0) {
+        if (arr[i][j]) arr1.push(arr[i][j]);
+        i -= 1;
+        j += 1;
+      }
+      result.push(arr1);
+    }
+  
+    for (let k = 1; k <= arr.length; k++) {
+      const arr2 = [];
+      let i = arr.length - 1;
+      let j = k;
+      while (i >= 0) {
+        if (arr[i][j]) arr2.push(arr[i][j]);
+        i -= 1;
+        j += 1;
+      }
+      if (arr.length > 0) result.push(arr2);
+    }
+    return result;
+  };
+  
+  module.exports = wordSearch;
